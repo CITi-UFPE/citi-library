5beb6bd19995ae992915acb65955a6e9cbfcf9fd
Vanessa Barreiros <vanessa@vinta.com.br>
2018-09-18 21:04:27 -0300

    Add .htaccess, change to absolute path

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ class App extends Component {
   render () {
     const { store } = this.props
     return (
-      <Router>
+      <Router basename='/library'>
         <Provider store={store}>
           <div className={styles.body}>
             <div className={styles.container}>
