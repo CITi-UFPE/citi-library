6f1554d5531ab592428be09046ac3a1ff520726d
Vanessa Barreiros <vanessa@vinta.com.br>
2019-05-25 01:37:51 -0300

    Fix removal from leaderboard

--- src/components/contributor/contributor.jsx
+++ src/components/contributor/contributor.jsx
@@ -3,27 +3,25 @@ import { connect } from 'react-redux'
 import styles from './style.scss'
 
 const Contributor = ({ author: { avatar, username }, count }) => {
-    return (
-        <div className={styles.contributorContainer}>
-            <div className={styles.author}>
-                <img
-                    className={styles.authorAvatar}
-                    src={avatar}
-                    alt={`Avatar de ${username}`}
-                />
-                {username}
-            </div>
-            <div className={styles.count}>{count} contribuiç{count === 1 ? 'ão' : 'ões'}</div>
-        </div>
-    );
+  return (
+    <div className={styles.contributorContainer}>
+      <div className={styles.author}>
+        <img className={styles.authorAvatar} src={avatar} alt={`Avatar de ${username}`} />
+        {username}
+      </div>
+      <div className={styles.count}>
+        {count} contribuiç{count === 1 ? 'ão' : 'ões'}
+      </div>
+    </div>
+  )
 }
 
 const mapStateToProps = (state, { authorId }) => {
-    const author = (
-      state.authors.items && state.authors.items.length > 0 &&
-      state.authors.items.find(i => i.id === authorId)
-    )
-    return { author: author ? author.data : {} }
+  const author =
+    state.authors.items &&
+    state.authors.items.length > 0 &&
+    state.authors.items.find((i) => i.id === authorId)
+  return { author: author ? author.data : {} }
 }
-  
+
 export default connect(mapStateToProps)(Contributor)
--- src/pages/leaderboard/leaderboard.jsx
+++ src/pages/leaderboard/leaderboard.jsx
@@ -8,7 +8,9 @@ import { connect } from 'react-redux'
 const MIN_COUNT = 2
 
 class Leaderboard extends Component {
-  groupContributors = (contributions) => {
+  groupContributors = contributions => {
+    const { authors } = this.props
+    const authorsLoaded = !!Object.keys(authors).length
     const contributors = contributions.reduce((collection, current) => {
       const { authorId } = current.data
       if (authorId in collection) collection[authorId]++
@@ -17,11 +19,15 @@ class Leaderboard extends Component {
     }, {})
 
     return Object.keys(contributors)
-      .map((author) => {
+      .map(author => {
         return { author, count: contributors[author] }
       })
       .sort((a, b) => b.count - a.count)
-      .filter(author => author.count >= MIN_COUNT && author.username !== 'vanessa')
+      .filter(author => {
+        // author.author is the id
+        const authorData = authorsLoaded && authors.items.find(a => a.id === author.author).data
+        return author.count >= MIN_COUNT && (authorData && authorData.username !== 'vanessa')
+      })
   }
 
   render() {
@@ -42,8 +48,9 @@ class Leaderboard extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({
-  library: state.library
+const mapStateToProps = state => ({
+  library: state.library,
+  authors: state.authors
 })
 
 export default connect(mapStateToProps)(Leaderboard)
