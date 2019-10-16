b4bf2a311d24d901c66e10dff2da723a88c98f50
Vanessa Barreiros <vanessa@vinta.com.br>
2019-10-15 23:51:57 -0300

    Corrige link do código fonte no footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,4 +1,4 @@
-import { GoOctoface } from 'react-icons/go'
+import { FaGithub } from 'react-icons/fa'
 import React from 'react'
 import styles from './style.scss'
 
@@ -7,17 +7,18 @@ const Footer = () => (
     <div className={styles.howTo}>
       <h4>Como usar?</h4>
       <div>
-        Tudo que você precisa fazer para utilizar o <code>/library</code> é
-        ir no canal #library e usar o comando abaixo (tags são opcionais)
-        <pre><code><strong>/library</strong> O que você quiser [tag1, tag2]</code></pre>
+        Tudo que você precisa fazer para utilizar o <code>/library</code> é ir no canal #library e
+        usar o comando abaixo (tags são opcionais)
+        <pre>
+          <code>
+            <strong>/library</strong> O que você quiser [tag1, tag2]
+          </code>
+        </pre>
       </div>
     </div>
     <div className={styles.author}>
-      Feito por <a href='//github.com/vanessa' rel='noopener noreferrer' target='_blank'>
-       @vanessa
-      </a>
-      <a className={styles.source} href='https://github.com/vanessa/citi-library'>
-        Código fonte <GoOctoface />
+      <a className={styles.source} href="https://github.com/CITi-UFPE/citi-library/">
+        Código fonte <FaGithub />
       </a>
     </div>
   </div>
